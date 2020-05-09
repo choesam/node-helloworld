d1ec56271f1a7cfce9eec276a8ca817ec481c189
choesam <choi3832>
2020-05-10 06:52:39 +0900

    setting for Es6

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let http = require("http");
-
-http.createServer(function(request, response){
-     response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    /*
-        Response Body 를 "Hello World" 로 설정
-    */
-    response.end("Hello World\n");
-}).listen(8081);
-
-console.log("Server running at http://127.0.0.1:8081");
+console.log("Hello,World");
